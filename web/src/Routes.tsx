966535aa08ebf81f810b75d2c3f747be4943596c
romanlp <roman.lapacherie@gmail.com>
2022-11-14 16:38:10 +0000

    create article page

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -21,10 +21,13 @@ const Routes = () => {
         <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
         <Route path="/posts" page={PostPostsPage} name="posts" />
       </Set>
+
       <Set wrap={BlogLayout}>
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
+        <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
       </Set>
+
       <Route notfound page={NotFoundPage} />
     </Router>
   );
